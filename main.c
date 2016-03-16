8eecef7c0713d6debb1b344f415ffed769de318a
Augusto Hoffmann <hoffmann.augusto@gmail.com>
2016-03-16 10:56:07 -0300

    Trabalho_Final_v2.0
    Inicio do trabalho com parte gráfica

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/**
+*   Arquivo main.c do trabalho final da disciplina de Programação 2
+*
+*   Instituto Federal de Santa Catarina
+*   Curso: Engenharia Eletrônica
+*   Disciplina: Programação 2
+*   Semestre 2015/2
+*   Professor: Daniel Lohmann
+*   Aluno: Augusto Hoffmann
+*
+*/
+
+#include "func_allegro.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
